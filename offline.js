4b06cc3162da0c42da31d637fd180f2dd2530a74
Gabriel Pires de Camargo <72245726+gabrielpirescamargo@users.noreply.github.com>
2021-11-23 15:41:46 -0300

    v1.0

---
+++ offline.js
@@ -0,0 +1,32 @@
+﻿{
+	"version": 1637681327,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/diagnostico-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite6-sheet0.png",
+		"images/dica1-sheet0.png",
+		"images/dica1-sheet1.png",
+		"images/menuinicial-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/x-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"images/sprite7-sheet0.png",
+		"images/sprite8-sheet0.png",
+		"images/voltarmenu-sheet0.png",
+		"images/sprite9-sheet0.png",
+		"images/sprite10-sheet0.png",
+		"images/sprite11-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
